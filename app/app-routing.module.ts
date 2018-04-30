8abdd16d178a2dbfb9ed419378365f45def5686d
Johannes Heinz <johannesmartinheinz@gmail.com>
2018-04-30 23:50:00 +0200

    Added login screen from groceries app

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
+
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/tabs", pathMatch: "full" },
+    { path: "", redirectTo: "/login", pathMatch: "full" },
+    { path: "login", loadChildren: "./login/login.module#LoginModule" },
     { path: "tabs", loadChildren: "./tabs/tabs.module#TabsModule" }
 ];
 
 @NgModule({
-    imports: [NativeScriptRouterModule.forRoot(routes)],
+    imports: [
+        NativeScriptRouterModule.forRoot(routes),
+        NativeScriptFormsModule,
+        NativeScriptHttpModule
+    ],
     exports: [NativeScriptRouterModule]
 })
 export class AppRoutingModule { }
---
+++ app/login/login.component.ts
@@ -0,0 +1,98 @@
+// Angular imports
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
+
+// NativeScript imports
+import { Color } from "color";
+import { Page } from "ui/page";
+import { View } from "ui/core/view";
+import { TextField } from "ui/text-field";
+import { isAndroid } from "platform";
+
+// App imports
+import { User } from "./user/user";
+import { UserService } from "./user/user.service";
+import { setHintColor } from "../utils/hint-util";
+
+
+@Component({
+    selector: "LoginComponent",
+    moduleId: module.id,
+    providers: [UserService],
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.scss"]
+})
+export class LoginComponent implements OnInit {
+
+    user: User;
+    isLoggingIn = true;
+
+    @ViewChild("container") container: ElementRef;
+    @ViewChild("email") email: ElementRef;
+    @ViewChild("password") password: ElementRef;
+
+    constructor(private router: Router, private userService: UserService, private page: Page) {
+        this.user = new User();
+        this.user.email = "nils@holgersson.se";
+        this.user.password = "Selma Lagerlöf";
+    }
+
+    ngOnInit() {
+        this.page.actionBarHidden = true;
+        this.page.backgroundImage = "res://bg_login";
+    }
+
+    submit() {
+        if (!this.user.isValidEmail()) {
+            alert("Enter a valid email address.");
+            return;
+        }
+        if (this.isLoggingIn) {
+            this.login();
+        } else {
+            this.signUp();
+        }
+    }
+
+    login() {
+        this.userService.login(this.user)
+            .subscribe(
+                () => this.router.navigate(["/tabs"]),
+                (error) => alert("Unfortunately we could not find your account.")
+            );
+    }
+
+    signUp() {
+        this.userService.register(this.user)
+            .subscribe(
+                () => {
+                    alert("Your account was successfully created.");
+                    this.toggleDisplay();
+                },
+                () => alert("Unfortunately we were unable to create your account.")
+            );
+    }
+
+    toggleDisplay() {
+        this.isLoggingIn = !this.isLoggingIn;
+        this.setTextFieldColors();
+        let container = <View>this.container.nativeElement;
+        container.animate({
+            backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
+            duration: 200
+        });
+    }
+
+    setTextFieldColors() {
+        let emailTextField = <TextField>this.email.nativeElement;
+        let passwordTextField = <TextField>this.password.nativeElement;
+
+        let mainTextColor = new Color(this.isLoggingIn ? "black" : "#C4AFB4");
+        emailTextField.color = mainTextColor;
+        passwordTextField.color = mainTextColor;
+
+        let hintColor = new Color(this.isLoggingIn ? "#ACA6A7" : "#C4AFB4");
+        setHintColor({ view: emailTextField, color: hintColor });
+        setHintColor({ view: passwordTextField, color: hintColor });
+    }
+}
