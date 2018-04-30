54b279f3d8772ba367838c565c31727b8d9835c3
Adrian Woerle <adrian.woerle@web.de>
2018-04-30 23:28:41 +0200

    Added two additonal tabs.

--- app/tabs/tabs.module.ts
+++ app/tabs/tabs.module.ts
@@ -3,10 +3,11 @@ import { NativeScriptCommonModule } from "nativescript-angular/common";
 
 import { BrowseComponent } from "./browse/browse.component";
 import { HomeComponent } from "./home/home.component";
+import { MapComponent } from "./map/map.component";
 import { SearchComponent } from "./search/search.component";
 import { TabsRoutingModule } from "./tabs-routing.module";
 import { TabsComponent } from "./tabs.component";
-
+import { WarehouseComponent } from "./warehouse/warehouse.component";
 @NgModule({
     imports: [
         NativeScriptCommonModule,
@@ -16,7 +17,9 @@ import { TabsComponent } from "./tabs.component";
         TabsComponent,
         HomeComponent,
         BrowseComponent,
-        SearchComponent
+        SearchComponent,
+        MapComponent,
+        WarehouseComponent
     ],
     schemas: [
         NO_ERRORS_SCHEMA
---
+++ app/tabs/warehouse/warehouse.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "Warehouse",
+    moduleId: module.id,
+    templateUrl: "./warehouse.component.html"
+})
+export class WarehouseComponent implements OnInit {
+    constructor() {
+        // Use the constructor to inject services.
+    }
+
+    ngOnInit(): void {
+        // Use the "ngOnInit" handler to initialize data for the view.
+    }
+}
