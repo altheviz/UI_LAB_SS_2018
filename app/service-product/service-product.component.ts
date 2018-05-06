c947c0cdffa14707b294d56a5c4d0cf5de1a8d6d
Adrian Woerle <>
2018-05-06 20:14:24 +0200

    Added Service Product Screen.

---
+++ app/service-product/service-product.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "ServiceProduct",
+    moduleId: module.id,
+    templateUrl: "./service-product.component.html"
+})
+export class ServiceProductComponent implements OnInit {
+
+    constructor() {
+        // Use the constructor to inject services.
+    }
+
+    ngOnInit(): void {
+        // Use the "ngOnInit" handler to initialize data for the view.
+    }
+}
