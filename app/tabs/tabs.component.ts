c40b275b5531b4733ba3a1c5e6fd1928155c07ed
Raphael Hippe <info@raphaelhippe.de>
2018-05-11 17:20:47 +0200

    added routing

--- app/tabs/tabs.component.ts
+++ app/tabs/tabs.component.ts
@@ -1,47 +1,58 @@
 import { Component, OnInit } from "@angular/core";
 import { isAndroid } from "platform";
 import { SelectedIndexChangedEventData, TabView, TabViewItem } from "tns-core-modules/ui/tab-view";
+import { PageRoute } from "nativescript-angular/router";
+import { switchMap } from "rxjs/operators";
 
 @Component({
-    selector: "TabsComponent",
-    moduleId: module.id,
-    templateUrl: "./tabs.component.html",
-    styleUrls: ["./tabs.component.scss"]
+  selector: "TabsComponent",
+  moduleId: module.id,
+  templateUrl: "./tabs.component.html",
+  styleUrls: ["./tabs.component.scss"]
 })
 export class TabsComponent implements OnInit {
 
-    private _title: string;
+  private _title: string;
 
-    constructor() {
-        // Use the component constructor to inject providers.
+  constructor(private pageRoute: PageRoute) {
+    // Use the component constructor to inject providers.
+
+    // use switchMap to get the latest activatedRoute instance
+    let self:any=this;
+    this.pageRoute.activatedRoute.pipe(
+      switchMap(activatedRoute => activatedRoute.params)
+    ).forEach((params) => {
+      console.log(params["tab"]);
+    });
+  }
+
+  ngOnInit(): void {
+    // Init your component properties here.
+
+  }
+
+  onNewServiceRequest() {
+    console.log("TODO: Call ServiceRequestComponent");
+  }
+
+  get title(): string {
+    return this._title;
+  }
+
+  set title(value: string) {
+    if (this._title !== value) {
+      this._title = value;
     }
+  }
 
-    ngOnInit(): void {
-        // Init your component properties here.
-    }
+  getIconSource(icon: string): string {
+    return isAndroid ? "" : "res://tabIcons/" + icon;
+  }
 
-    onNewServiceRequest() {
-        console.log("TODO: Call ServiceRequestComponent");
-    }
+  onSelectedIndexChanged(args: SelectedIndexChangedEventData) {
+    const tabView = <TabView>args.object;
+    const selectedTabViewItem = tabView.items[args.newIndex];
 
-    get title(): string {
-        return this._title;
-    }
-
-    set title(value: string) {
-        if (this._title !== value) {
-            this._title = value;
-        }
-    }
-
-    getIconSource(icon: string): string {
-        return isAndroid ? "" : "res://tabIcons/" + icon;
-    }
-
-    onSelectedIndexChanged(args: SelectedIndexChangedEventData) {
-        const tabView = <TabView>args.object;
-        const selectedTabViewItem = tabView.items[args.newIndex];
-
-        this.title = selectedTabViewItem.title;
-    }
+    this.title = selectedTabViewItem.title;
+  }
 }
