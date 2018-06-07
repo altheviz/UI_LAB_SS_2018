848ae7835ad20ad8026fbc2a899f37024e0d4a55
Johannes Heinz <johannesmartinheinz@gmail.com>
2018-06-07 21:12:48 +0200

    Added intents for email and telephone

--- app/tabs/customers/customer-detail/customer-detail.component.ts
+++ app/tabs/customers/customer-detail/customer-detail.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { registerElement } from "nativescript-angular/element-registry";
 import { CardView } from "nativescript-cardview";
+import { compose } from "nativescript-email";
+import { dial } from "nativescript-phone";
 import { ActionBar } from "tns-core-modules/ui/action-bar/action-bar";
+import { GestureEventData } from "ui/gestures";
 import { openUrl } from "utils/utils";
 import { Customer } from "~/models/customer";
 import { DummyService } from "~/models/dummy.service";
@@ -29,18 +32,43 @@ export class CustomerComponent implements OnInit {
     this.active = this.customers.filter((c) => c.id === this.id)[0];
   }
 
-  callAddressNumber() {
-    console.log("call number of the company");
+  openMaps() {
+    console.log("Open company site");
   }
 
-  openMaps() {
-    console.log("open map");
+  goToHomepage(args: GestureEventData) {
+    console.log("Open company homepage");
+    console.log("Tap!");
+    console.log("Object that triggered the event: " + args.object);
+    console.log("View that triggered the event: " + args.view);
+    console.log("Event name: " + args.eventName);
+    // openUrl(homepage);
   }
-  goToHomepage() {
-    openUrl(this.active.homepage);
+
+  callAddressNumber(args: GestureEventData) {
+    console.log("Call number of the company");
+    console.log("Tap!");
+    console.log("Object that triggered the event: " + args.object);
+    console.log("View that triggered the event: " + args.view);
+    console.log("Event name: " + args.eventName);
+    //dial(telephone, true);
   }
-  sendAddressMail() {
-    console.log("send mail");
+
+  sendAddressMail(args: GestureEventData) {
+    console.log("Send email to company");
+    console.log("Tap!");
+    console.log("Object that triggered the event: " + args.object);
+    console.log("View that triggered the event: " + args.view);
+    console.log("Event name: " + args.eventName);
+    /*
+    compose({
+      to: [ email ]
+    }).then(() => {
+      console.log("Email composer closed");
+    }, (err) => {
+      console.log("Error: " + err);
+    });
+    */
   }
 
   ngOnInit(): void {}
