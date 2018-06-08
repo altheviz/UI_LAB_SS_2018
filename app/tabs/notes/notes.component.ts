ee16ce457fcdaece986c19d9a96e7fd7cc85d031
Tobias Kerst <tobias.kerst@tobscore.com>
2018-06-08 10:22:59 +0200

    Optimizes todo list layouting

--- app/tabs/notes/notes.component.ts
+++ app/tabs/notes/notes.component.ts
@@ -14,6 +14,7 @@ class DataItem {
     providers: [DummyService],
     moduleId: module.id,
     templateUrl: "./notes.component.html",
+    styleUrls: ["./notes.component.scss"],
 	changeDetection: ChangeDetectionStrategy.OnPush
 
 })
