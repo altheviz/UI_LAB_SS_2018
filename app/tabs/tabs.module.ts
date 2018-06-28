2b92ca7ebb08b3c2318fde7c27d7acce4f79ddea
Johannes Heinz <johannesmartinheinz@gmail.com>
2018-06-28 18:21:56 +0200

    Styling + service improvement (completion)

--- app/tabs/tabs.module.ts
+++ app/tabs/tabs.module.ts
@@ -1,23 +1,23 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { ModalDialogService } from "nativescript-angular/modal-dialog";
 import { DropDownModule } from "nativescript-drop-down/angular";
-
+import { ContentService } from "~/models/content.service";
+import { ServiceCompletionPartManager } from "~/models/serviceCompletionPartsManager";
+import { AppointmentDetailComponent } from "~/tabs/appointments/appointment-detail/appointment-detail.component";
 import { AppointmentsComponent } from "~/tabs/appointments/appointments.component";
+import { CompletionComponent } from "~/tabs/appointments/completion/completion.component";
+import { ServiceRequestComponent } from "~/tabs/appointments/servicerequest/servicerequest.component";
+import { CustomerComponent } from "~/tabs/customers/customer-detail/customer-detail.component";
 import { CustomersComponent } from "~/tabs/customers/customers.component";
 import { MapComponent } from "~/tabs/map/map.component";
+import { NoteDetailComponent } from "~/tabs/notes/note-detail/note-detail.component";
 import { NotesComponent } from "~/tabs/notes/notes.component";
 import { TabsRoutingModule } from "~/tabs/tabs-routing.module";
 import { TabsComponent } from "~/tabs/tabs.component";
 import { WarehouseComponent } from "~/tabs/warehouse/warehouse.component";
 import { ListComponent } from "~/utils/list/list.component";
-
-import { ModalDialogService } from "nativescript-angular/modal-dialog";
-import { AppointmentDetailComponent } from "~/tabs/appointments/appointment-detail/appointment-detail.component";
-import { CompletionComponent } from "~/tabs/appointments/completion/completion.component";
-import { ServiceRequestComponent } from "~/tabs/appointments/servicerequest/servicerequest.component";
-import { CustomerComponent } from "~/tabs/customers/customer-detail/customer-detail.component";
-import { NoteDetailComponent } from "~/tabs/notes/note-detail/note-detail.component";
 import { NotesModalComponent } from "./notes/notes.modal";
 // import { ServiceProductComponent } from "~/service-product/service-product.component";
 
@@ -51,7 +51,9 @@ import { NotesModalComponent } from "./notes/notes.modal";
         NO_ERRORS_SCHEMA
     ],
     providers: [
-        ModalDialogService
+        ModalDialogService,
+        ContentService,
+        ServiceCompletionPartManager
     ]
 })
 export class TabsModule { }
